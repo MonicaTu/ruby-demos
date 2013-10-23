a72a98d89b2c65f146622b0802b808008363202e
Monica Tu <monica.service@gmail.com>
2013-10-23 13:29:11 +0800

    (demo_ai4r_kmeans.rb) Initial.

---
+++ demo_ai4r_kmeans.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'ai4r/clusterers/k_means'
+
+include Ai4r::Clusterers
+include Ai4r::Data
+
+
+@@data = [  [10, 3], [3, 10], [2, 8], [2, 5], [3, 8], [10, 3],
+            [1, 3], [8, 1], [2, 9], [2, 5], [3, 3], [9, 4]]
+
+data_set = DataSet.new(:data_items => @@data, :data_labels => ["X", "Y"])
+clusterer = KMeans.new.build(data_set, 4)
+clusterer.clusters.each do |cluster|
+  p cluster.data_items
+end

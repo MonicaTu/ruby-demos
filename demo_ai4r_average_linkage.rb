b3446032acc333c11e423f23660ba29cf0f2a649
monica <monica@ubuntu.ubuntu-domain>
2013-10-25 19:06:55 +0800

    (demo_ai4r_average_linkage) Read distance_matrix and save to spreadsheet file.

--- demo_ai4r_average_linkage.rb
+++ demo_ai4r_average_linkage.rb
@@ -5,8 +5,8 @@ require 'ai4r/clusterers/average_linkage'
 include Ai4r::Clusterers
 include Ai4r::Data
 
-@@data = [  [10, 3], [3, 10], [2, 8], [2, 5], [3, 8], [10, 3],
-            [1, 3], [8, 1], [2, 9], [2, 5], [3, 3], [9, 4]]
+@@data = [  [10, 3.10], [3, 10.3], [2, 8.2], [2, 5.2], [3, 8.3], [10, 3.1],
+            [1, 3.1], [8, 1.8], [2, 9.2], [2, 5.2], [3, 3.3], [9, 4.9]]
 
 #=== demo1 ===
 def demo1
@@ -21,7 +21,7 @@ def demo1
   end
 end
 
-#=== demo2 ===
+#=== demo2-1 ===
 def create_distance_matrix(data_set)
   @distance_function = lambda do |a,b|
   Ai4r::Data::Proximity.squared_euclidean_distance(
@@ -38,13 +38,43 @@ def create_distance_matrix(data_set)
   end
 end
 
-def demo2
-  p "=== demo2 ==="
+def demo2a
+  p "=== demo2a ==="
   data_set = DataSet.new(:data_items => @@data, :data_labels => ["X", "Y"])
   
   create_distance_matrix(data_set)
   p @distance_matrix
 end
 
+#=== demo2-2 ===
+require 'spreadsheet'
+
+def read_distance_matrix(index_a, index_b)
+  return 0 if index_a == index_b
+  index_a, index_b = index_b, index_a if index_b > index_a
+  return @distance_matrix[index_a-1][index_b]
+end
+
+def demo2b
+  p "=== demo2b ==="
+  book = Spreadsheet::Workbook.new
+  sheet = book.create_worksheet
+  sheet.name = 'My First Worksheet'
+  
+  sheet.row(0).concat %w{g1 g2 g3}
+  sheet[2,0] = read_distance_matrix(1, 0) 
+  sheet[3,0] = read_distance_matrix(2, 0) 
+  sheet[4,0] = read_distance_matrix(3, 0) 
+  sheet[5,0] = read_distance_matrix(4, 0) 
+
+  sheet[1,1] = sheet[2,0]
+  sheet[1,2] = sheet[3,0]
+  sheet[1,3] = sheet[4,0]
+  sheet[1,4] = sheet[5,0]
+  
+  book.write 'excel-ai4r_average_linkage.xls'
+end
+
 #demo1
-demo2
+demo2a
+demo2b
